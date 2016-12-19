c265411e09768f416cd1714a71b8c7e69f3529e5
Lovro Kordiš <lovro.kordis@fer.hr>
2016-12-19 18:39:07 +0100

    Dodan confirmation mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+  def after_confirmation_path_for(resource_name, resource)
+    recipes_path
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206230924) do
+ActiveRecord::Schema.define(version: 20161219162530) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title",              limit: 255
@@ -39,8 +39,12 @@ ActiveRecord::Schema.define(version: 20161206230924) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
